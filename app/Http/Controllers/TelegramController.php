246c307f2a873380ca71cdb5a1fc39a1dd60c3cc
Goldowansky <goldowansky@gmail.com>
2025-10-26 20:04:03 +0200

    Added class selection

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use Illuminate\Http\Request;
 use TelegramBot\Api\BotApi;
 use Illuminate\Support\Facades\Log;
+use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
 
 class TelegramController extends Controller
 {
@@ -13,6 +15,7 @@ class TelegramController extends Controller
         $telegram = new BotApi(env('TELEGRAM_BOT_TOKEN'));
         $update = $request->all();
         $message = $update['message'] ?? null;
+        $callbackQuery = $update['callback_query'] ?? null;
 
         if ($message) 
         {
@@ -21,12 +24,32 @@ class TelegramController extends Controller
             $responcemessage = "";
             if($text=="/start")
             {
-                $responcemessage = "Привіт, це стартове повідомлення";
+                $responcemessage = "Вітаю, оберіть клас!";
+                $grades = Group::distinct()->pluck('grade')->sort()->toArray();
+                $buttons =  [];
+                $row = [];
+                for ($i = 0; $i < count($grades); $i++) {
+                    $row[] = ['text' => $grades[$i], 'callback_data' => 'select_class_' . $grades[$i]];
 
+                    if ($i % 3 == 2) {
+                        $buttons[] = $row;
+                        $row = [];
+                    }
+                }
+
+                if (count($row) > 0) {
+                    $buttons[] = $row;
+                }
+
+                $keyboard = new InlineKeyboardMarkup($buttons);
+                $telegram->sendMessage($chatId, $responcemessage, null, false, null, $keyboard, null, false);
             } else {
                 $responcemessage = "Не знаю такої команди";
+                $telegram->sendMessage($chatId, $responcemessage);
             }
-            $telegram->sendMessage($chatId, $responcemessage);
+        } elseif ($callbackQuery) {
+            $chatId = $callbackQuery['message']['chat']['id'];
+            $telegram->sendMessage($chatId, 'OK, ' . $callbackQuery['data']);
         }
     }
 
