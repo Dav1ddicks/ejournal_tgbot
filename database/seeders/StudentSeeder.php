34bc3d892495e092a3d9c9dace09e12298eac49a
Goldowansky <goldowansky@gmail.com>
2025-10-26 17:39:44 +0200

    Fixed middle name in seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -3,9 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Group;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Faker\Provider\Person;
 
 class StudentSeeder extends Seeder
 {
@@ -14,16 +14,21 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
+        // Створення об'єкта Faker з українською локалізацією
         $faker = Faker::create('uk_UA');
         $groups = Group::all(); // Всі групи, які були створені в GroupSeeder
 
         foreach ($groups as $group) {
             $numOfStudents = rand(25, 30); // В кожній групі від 25 до 30 учнів
             for ($i = 0; $i < $numOfStudents; $i++) {
+                // Вибір випадкового гендеру
+                $gender = $faker->randomElement([Person::GENDER_MALE, Person::GENDER_FEMALE]);
+
+                // Генерація даних студента
                 $group->students()->create([
-                    'first_name'    => $faker->firstName,
-                    'mid_name'      => $faker->firstName,
-                    'last_name'     => $faker->lastName,
+                    'first_name'    => $faker->firstName($gender), // Використовуємо метод firstName для генерації імені за гендером
+                    'mid_name'      => $faker->middleName($gender), // Використовуємо middleName для по-батькові
+                    'last_name'     => $faker->lastName, // Генерація прізвища
                 ]);
             }
         }
